dae5d82cae67592aab29df8f4364f67f984ec42b
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-22 23:35:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,6 +49,12 @@ end
 #####################################################
 
 def runner
-  
+  welcome 
+  initial_round 
+  hit 
+  display_card_total
+  until card_total > 21 
+end_game
+end
 end
     
