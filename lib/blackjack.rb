4f0e3f65cfda2f29a3a6a9313ee5a57f2db8aab3
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-22 22:53:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,13 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
 def display_card_total
-  # code #display_card_total here
+ puts "Type 'h' to hit or 's' to stay"
 end
 
 def prompt_user
